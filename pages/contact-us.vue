bb3866fa8947145a487b1c8b3806c3de4e61101c
lieta96 <julieta.garcia96@gmai.com>
2021-12-07 14:22:10 -0300

    actualizacion pagina de contacto

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -1,10 +1,31 @@
 <template>
-  <div class="hero is-large">
-    <div class="hero-body">
+  <div>
+    <div class="section my-6">
       <div class="container">
-        <h1 class="title is-1 has-text-centered is-size-4-touch">
-          Coming soon
-        </h1>
+        <div class="columns is-centered">
+          <div class="column is-8">
+            <h1 class="title is-3 has-text-centered">
+              {{ $t('contact.head.title') }}
+            </h1>
+            <p class="has-text-centered is-size-5">
+              {{ $t('contact.title') }}
+            </p>
+            <div class="mt-6 is-flex is-justify-content-space-evenly">
+              <a href="https://www.facebook.com/CCIJustice"><i class="fab fa-facebook-f fa-fw is-size-3" /></a>
+              <a href="https://www.instagram.com/ccijustice/"><i class="fab fa-instagram fa-fw is-size-3" /></a>
+              <a href="https://twitter.com/ccijustice"><i class="fab fa-twitter fa-fw is-size-3" /></a>
+              <a href="https://www.linkedin.com/uas/login?session_redirect=https%3A%2F%2Fwww.linkedin.com%2Fcompany%2F65597301%2Fadmin%2F"><i class="fab fa-linkedin fa-fw is-size-3" /></a>
+            </div>
+            <div class="content" v-html="text" />
+          </div>
+        </div>
+         <div class="columns is-centered">
+            <div class="column is-6">
+              <hr>
+              <a href="https://ccijustice.org/contact-us/" class="mt-6 button is-primary is-outlined is-fullwidth is-uppercase">{{ $t('contact.buttonQuestions') }}</a>
+              <a href="https://ccijustice.org/contact-us/" class="py-2 my-4 button is-primary is-outlined is-fullwidth is-uppercase">{{ $t('contact.buttonPlatform') }}</a>
+            </div>
+          </div>
       </div>
     </div>
   </div>
@@ -12,6 +33,26 @@
 
 <script>
 export default {
+  async asyncData ({ params, $axios, i18n }) {
+    const theQuery = {
+      query: `
+      {
+        contact_translations(filter: {languages_code: {id: {_eq: "${i18n.locale}"}}}){
+          text
+        }
+      } 
+      `
+    }
+    try {
+      const response = await $axios.post('/graphql', theQuery)
+      return {
+        text: response.data.data.contact_translations[0].text
+      }
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.error(err.response)
+    }
+  },
   head () {
     const i18nHead = this.$nuxtI18nHead({ addSeoAttributes: true })
     return {
@@ -36,3 +77,9 @@ export default {
   }
 }
 </script>
+<style lang="scss">
+a.button{
+  white-space: break-spaces;
+  height: auto;
+}
+</style>
