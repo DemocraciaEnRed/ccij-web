39fd35b65c2e5c80d7e55f7992404311ea86bd40
Guillermo Croppi <guillermocroppi@gmail.com>
2022-04-07 12:57:10 -0300

    Added Tracking to Footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -7,10 +7,10 @@
             <h1 class="subtitle is-700 is-3 has-text-white">
               {{ $t('footer.ourNetworks') }}
             </h1>
-            <a class="has-text-light" href="https://www.facebook.com/CCIJustice"><i class="fab fa-facebook-f fa-fw is-size-3" /></a>
-            <a class="has-text-light" href="https://www.instagram.com/ccijustice/"><i class="fab fa-instagram fa-fw is-size-3" /></a>
-            <a class="has-text-light" href="https://twitter.com/ccijustice"><i class="fab fa-twitter fa-fw is-size-3" /></a>
-            <a class="has-text-light" href="https://www.linkedin.com/uas/login?session_redirect=https%3A%2F%2Fwww.linkedin.com%2Fcompany%2F65597301%2Fadmin%2F"><i class="fab fa-linkedin fa-fw is-size-3" /></a>
+            <a class="has-text-light" @click="trackAndGoToWebsite('https://www.facebook.com/CCIJustice','Facebook')"><i class="fab fa-facebook-f fa-fw is-size-3" /></a>
+            <a class="has-text-light" @click="trackAndGoToWebsite('https://www.instagram.com/ccijustice/','Instagram')"><i class="fab fa-instagram fa-fw is-size-3" /></a>
+            <a class="has-text-light" @click="trackAndGoToWebsite('https://twitter.com/ccijustice','Twitter')"><i class="fab fa-twitter fa-fw is-size-3" /></a>
+            <a class="has-text-light" @click="trackAndGoToWebsite('https://www.linkedin.com/uas/login?session_redirect=https%3A%2F%2Fwww.linkedin.com%2Fcompany%2F65597301%2Fadmin%2F','LinkedIn')"><i class="fab fa-linkedin fa-fw is-size-3" /></a>
           </div>
           <div class="column">
             <h1 class="subtitle is-700 is-3 has-text-white has-text-centered-mobile">
@@ -25,13 +25,13 @@
       <div class="container">
         <div class="columns is-centered is-vcentered">
           <div class="column is-7">
-            <a href="https://ccijustice.org/" target="_blank">
+            <a @click="trackAndGoToWebsite('https://ccijustice.org/','CCIJ Logo')">
               <img src="~/assets/img/ccij-logo-horizontal.svg" width="600" class="image mx-auto px-3">
             </a>
           </div>
           <div class="column is-3">
             <div class="buttons is-centered">
-              <a href="https://ccijustice.org/" target="_blank" class="button is-petroleo is-outlined is-uppercase">{{ $t('footer.goToSite') }} <i class="ml-3 fas fa-lg fa-arrow-right"></i></a>
+              <a class="button is-petroleo is-outlined is-uppercase" @click="trackAndGoToWebsite('https://ccijustice.org/','Go to website button')">{{ $t('footer.goToSite') }} <i class="ml-3 fas fa-lg fa-arrow-right" /></a>
             </div>
           </div>
         </div>
@@ -46,21 +46,13 @@ import FooterCampaigns from './FooterCampaigns.vue'
 export default {
   components: {
     FooterCampaigns
+  },
+  methods: {
+    trackAndGoToWebsite (url, where) {
+      this.$ga.event('Footer', 'Click', where)
+      // open in a new tab https://ccijustice.org/
+      window.open(url, '_blank')
+    }
   }
 }
 </script>
-
-<style lang="scss">
-// footer.footer{
-//   color: #000;
-//   .title{
-//     color: #000 !important;
-//   }
-//   a{
-//     color: #000;
-//     &:hover{
-//       color: white;
-//     }
-//   }
-// }
-</style>
